c8e40031e4e921c08646f982faa8354f7104f66e
YuNa <dbsk4030@gmail.com>
2025-04-07 14:39:38 +0900

    test

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,31 +1,31 @@
 import * as repository from '../repository/mainRepository.js';
 
 /* 메인 베스트 상품 리스트 호출 */
+export const getMainBestItem = async(req, res) => {
+    const result = await repository.getMainBestItem(req.body);
+    res.json(result);
+    res.end();
+}
+
+// /* 메인 선케어 상품 리스트 호출 */
+export const getMainSunItem = async(req, res) => {
+    const result = await repository.getMainSunItem(req.body);
+    res.json(result);
+    res.end();
+}
+
+// /* 메인 베스트 상품 리스트 호출 */
 // export const getMainBestItem = async(req, res) => {
-//     const result = await repository.getMainBestItem(req.body);
-//     res.json(result);
+//     // const result = await repository.getMainBestItem(req.body);
+//     // res.json(result);
+//     res.json({test:"test"});
 //     res.end();
 // }
 
 // /* 메인 선케어 상품 리스트 호출 */
 // export const getMainSunItem = async(req, res) => {
-//     const result = await repository.getMainSunItem(req.body);
-//     res.json(result);
+//     // const result = await repository.getMainSunItem(req.body);
+//     // res.json(result);
+//     res.json({test:"test"});
 //     res.end();
 // }
-
-/* 메인 베스트 상품 리스트 호출 */
-export const getMainBestItem = async(req, res) => {
-    // const result = await repository.getMainBestItem(req.body);
-    // res.json(result);
-    res.json({test:"test"});
-    res.end();
-}
-
-/* 메인 선케어 상품 리스트 호출 */
-export const getMainSunItem = async(req, res) => {
-    // const result = await repository.getMainSunItem(req.body);
-    // res.json(result);
-    res.json({test:"test"});
-    res.end();
-}
